3e25cbff721e2a5eef65da519d8074ae2c680469
PajkeGit <darth.pajke@gmail.com>
2023-08-04 05:11:47 +0200

    test

--- src/Components/TheGameSections.tsx
+++ src/Components/TheGameSections.tsx
@@ -12,7 +12,7 @@ const SectionRussianRoulette: React.FC = () => {
   return (
     <div
       id="russian-roulette"
-      className="flex flex-col w-screen justify-center md:flex-row mx-auto items-center gap-10 p-10 md:p-24 bg-[#100d0a] text-slate-200"
+      className="flex flex-col w-screen justify-center md:flex-row mx-auto items-center p-10 md:p-24 bg-[#100d0a] text-slate-200"
     >
       <motion.div className="flex justify-center">
         <Image
@@ -23,7 +23,7 @@ const SectionRussianRoulette: React.FC = () => {
           className="rounded-lg"
         />
       </motion.div>
-      <div>
+      <div className="md:ml-10">
         <h1 className="text-5xl font-bold mb-4">Russian Roulette</h1>
         <h2 className="text-3xl font-semibold mb-4">But on Telegram</h2>
         <ul className="list-inside list-decimal">
@@ -59,7 +59,7 @@ const SectionTelegramTwist: React.FC = () => {
   return (
     <div
       id="telegram-twist"
-      className="flex flex-col md:flex-row mx-auto items-center gap-10 p-10 md:p-24 bg-[#100d0a] text-slate-200"
+      className="w-screen justify-center flex flex-col md:flex-row mx-auto items-center gap-gi10 p-10 md:p-24 bg-[#100d0a] text-slate-200"
     >
       <div>
         <h1 className="text-5xl font-bold mb-8">The Telegram Twist</h1>
